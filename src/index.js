77d420a208ff178210c0f82c57469349eb070e83
Fakrudeen Ali T <fakrudeen.t@zohocorp.com>
2019-06-08 12:32:50 +0530

    adding existing files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    state = {
+        time: 0,
+        running: false
+    };
+    handleStartStop = () => {
+        this.setState(state => {
+            if (!state.running) {
+                const startTime = Date.now() - state.time;
+                this.timer = setInterval(() => {
+                    this.setState({
+                        time: Date.now() - startTime
+                    });
+                }, 0);
+            } else {
+                clearInterval(this.timer);
+            }
+            return { running: !state.running };
+        });
+    };
+    handleClear = () => {
+        clearInterval(this.timer);
+        this.setState({
+            time: 0,
+            running: false
+        });
+    };
+    render() {
+        return (
+            <div>
+                <div className="timer">{this.state.time}ms</div>
+                <button onClick={this.handleStartStop}>
+                    {this.state.running ? "Stop" : "Start"}
+                </button>
+                <button onClick={this.handleClear}>Clear</button>
+            </div>
+        );
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
